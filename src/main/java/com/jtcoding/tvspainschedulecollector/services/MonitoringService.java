8a2b83f88e0ce03abf9cdb7da603936f8ba8494f
Jaime Torres Benavente <jaime.torres.benavente@apipana.es>
2024-11-11 09:26:40 +0100

    metrics updated

--- src/main/java/com/jtcoding/tvspainschedulecollector/services/MonitoringService.java
+++ src/main/java/com/jtcoding/tvspainschedulecollector/services/MonitoringService.java
@@ -4,24 +4,30 @@ import com.jtcoding.tvspainschedulecollector.respositories.MovieRepository;
 import com.jtcoding.tvspainschedulecollector.respositories.SerieRepository;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Slf4j
 public class MonitoringService {
     private final MovieRepository movieRepository;
     private final SerieRepository serieRepository;
     private final MeterRegistry meterRegistry;
 
+    private final AtomicLong movies = new AtomicLong();
+    private final AtomicLong series = new AtomicLong();
+
     public void setMoviesMetrics(){
-        meterRegistry.gauge("movies",new AtomicLong(movieRepository.count()));
+        movies.set(movieRepository.count());
+        meterRegistry.gauge("movies",movies);
     }
 
     public void setSeriesMetrics(){
-        meterRegistry.gauge("series",new AtomicLong(serieRepository.count()));
+        series.set(serieRepository.count());
+        meterRegistry.gauge("series",series);
     }
 }
